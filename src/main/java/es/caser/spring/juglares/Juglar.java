b83f43a4ac35038ad4c3ff0cb84ee6d2c1a67cdb
DALOSA01 <DALOSA01@CASER-5041.caser.local>
2017-08-23 11:18:52 +0200

    Ejemplo 2: aop con juglar

---
+++ src/main/java/es/caser/spring/juglares/Juglar.java
@@ -0,0 +1,14 @@
+package es.caser.spring.juglares;
+
+
+
+public class Juglar {
+    public Juglar() {
+    }
+    public void cantarAntesMision() {
+        System.out.println ("Fa, la la... El caballero es valiente!");
+    }
+    public void cantarDespuesMision() {
+         System.out.println ("El caballero pone rumbo a su mision!");
+    }
+}
